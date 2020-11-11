b578f0d6e4584ef2fb383971b1df8650b33c7419
Lincoln Stein <lincoln.stein@gmail.com>
2020-11-11 16:12:04 -0500

    fixed torontoDSB, which added a new iframe and changed URL

--- lib/CovidSchools/SchoolScraper/torontoDSB.pm
+++ lib/CovidSchools/SchoolScraper/torontoDSB.pm
@@ -9,15 +9,19 @@ sub new {
     my $class = shift;
     return $class->SUPER::new(
 	DISTRICT => 'Toronto DSB',
-	URL      => 'https://www.tdsb.on.ca/Return-to-School/covid-19-advisories'
+	#	URL      => 'https://www.tdsb.on.ca/Return-to-School/covid-19-advisories'
+	URL => 'https://docs.google.com/spreadsheets/d/1gEipMl79REabV5GPuJnPeziC3DbYhA92U_BxpzdKX_Y/htmlembed/sheet?gid=0&',
 	);
 }
 
 sub table_fields {
-    return ('School',
-	    'Confirmed Cases Among Students',
-	    'Confirmed Cases Among Staff',
-	    'Open / Closed');
+    return (
+	'School',
+	'Students',
+	'Staff',
+	'Resolved',
+	'Open'
+	);
 }
 
 1;
